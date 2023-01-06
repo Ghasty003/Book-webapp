387c4699c257ade01c94eb783736d4803b8b03b8
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-06 17:47:21 -0400

    updated controller functions

--- backend/controller/bookController.js
+++ backend/controller/bookController.js
@@ -1,9 +1,30 @@
 const Book = require("../model/bookModel");
+const mongoose = require("mongoose");
 
 const getBooks = async (req, res) => {
   try {
     const books = await Book.find({}).sort({ createdAt: -1 });
     res.status(200).json(books);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+const getBook = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "Invalid book id" });
+  }
+
+  try {
+    const book = await Book.findOne({ id });
+
+    if (!book) {
+      return res.status(404).json({ message: "No such book" });
+    }
+
+    res.status(200).json(book);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
@@ -20,7 +41,51 @@ const createBook = async (req, res) => {
   }
 };
 
+const updateBook = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "Invalid book id" });
+  }
+
+  try {
+    const book = await Book.findOneAndUpdate(
+      { id },
+      {
+        ...req.body,
+      }
+    );
+
+    res.status(200).json(book);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
+
+const deleteBook = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "Invalid book id" });
+  }
+
+  try {
+    const book = await Book.findOneAndDelete({ id });
+
+    if (!book) {
+      return res.status(404).json({ message: "No such book" });
+    }
+
+    res.status(200).json(book);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
 module.exports = {
   getBooks,
+  getBook,
   createBook,
+  updateBook,
+  deleteBook,
 };
