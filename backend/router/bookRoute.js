4a2b51be663fc76e48b3b7878cba56af6a9cb33c
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-06 17:53:29 -0400

    updated routes

--- backend/router/bookRoute.js
+++ backend/router/bookRoute.js
@@ -1,10 +1,22 @@
 const express = require("express");
-const { createBook, getBooks } = require("../controller/bookController");
+const {
+  createBook,
+  getBooks,
+  getBook,
+  updateBook,
+  deleteBook,
+} = require("../controller/bookController");
 
 const router = express.Router();
 
 router.get("/", getBooks);
 
+router.get("/:id", getBook);
+
 router.post("/", createBook);
+
+router.patch("/:id", updateBook);
+
+router.delete("/:id", deleteBook);
 
 module.exports = router;
