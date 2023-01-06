a36369de2f145c80e868d7f0e05b994201006605
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-06 18:04:48 -0400

    added admin home page

---
+++ frontend/src/pages/AdminHomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AdminHomePage() {
+    return (
+        <div>
+            Admin Home page
+        </div>
+    );
+}
+
+export default AdminHomePage;
---
+++ frontend/src/pages/UserHomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function UserHomePage() {
+    return (
+        <div>
+            User home page
+        </div>
+    );
+}
+
+export default UserHomePage;
