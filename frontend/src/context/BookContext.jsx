faebb400d74202c7c2281850c1bfe929a3af22cd
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-07 15:29:51 -0400

    added fetch and add books logic

--- frontend/src/context/BookContext.jsx
+++ frontend/src/context/BookContext.jsx
@@ -1,15 +1,35 @@
+import { useReducer } from "react";
 import { createContext } from "react";
 
 
 const BookContext = createContext();
 
 
+const bookReducer = (state, action) => {
+    switch (action.type) {
+        case "FETCH BOOKS":
+            return {
+                books: action.payload
+            }
+        case "ADD BOOK":
+            return {
+                books: [action.payload, ...state.books]
+            }
+        
+        default:
+            return state;
+    }
+}
+
+
 export const BookContextProvider = ({ children }) => {
 
-    console.log("Book init")
+    const [state, dispatch] = useReducer(bookReducer, {
+        books: null
+    })
 
     return (
-        <BookContext.Provider value={{}}>
+        <BookContext.Provider value={{...state, dispatch}}>
             { children }
         </BookContext.Provider>
     )
