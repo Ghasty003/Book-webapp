6704ac4a033873d9ef348bcd1e263f5797657b22
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-06 21:42:53 -0400

    updated user model

---
+++ backend/model/userModel.js
@@ -0,0 +1,19 @@
+const { Schema, default: mongoose } = require("mongoose");
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  userName: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("users", userSchema);
