daa421285f9881bb5635fe5bc888ce8606f85665
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-07 18:31:09 -0400

    extends image limits

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const userRoute = require("./router/userRoute");
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use("/api/books", bookRoute);
 app.use("/api/users", userRoute);
 
