312e993eec21d8ab81f29548c544444a1b2c5176
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-06 22:05:45 -0400

    added signup route

--- backend/router/userRoute.js
+++ backend/router/userRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const { signupUser } = require("../controller/userController");
 
 const router = express.Router();
 
 router.post("/login");
 
-router.post("/signup");
+router.post("/signup", signupUser);
 
 module.exports = router;
