262c17d0e8c523f6caad3b91fa9ef347e0b21ad2
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-07 16:18:52 -0400

    updated book card layout

--- frontend/src/components/BookCard.jsx
+++ frontend/src/components/BookCard.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 function BookCard({ book }) {
     return (
-        <div className='flex items-center gap-3 my-2 bg-primary p-5 rounded-lg drop-shadow-2xl text-white'>
-            <img className='w-20' src={ book.image } alt="book" />
+        <div className='flex w-56 items-center my-2 py-4 pr-3 bg-primary rounded-lg drop-shadow-2xl text-white'>
+            <img className='w-36 h-20 object-contain drop-shadow-2xl' src={ book.image } alt="book" />
             <div>
                 <p className='font-bold text-lg'>{ book.authorName }</p>
-                <p>{ book.bookName }</p>
+                <p className='text-sm whitespace-nowrap'>{ book.bookName }</p>
             </div>
         </div>
     );
