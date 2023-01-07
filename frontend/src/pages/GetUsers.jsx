c8524232947619291634a8f70ce4bc8edabc9288
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-07 19:31:57 -0400

    completed get users page

--- frontend/src/pages/GetUsers.jsx
+++ frontend/src/pages/GetUsers.jsx
@@ -1,9 +1,56 @@
 import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import Nav from '../components/Nav';
+
+
+const Div = ({ user }) => {
+    return (
+        <div className='flex justify-between my-5'>
+            <p className='text-start'>{ user.email }</p>
+            <p>{ user.userName }</p>
+        </div>
+    )
+}
 
 function GetUsers() {
+
+    const [users, setUsers] = useState("");
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            const response = await fetch("http://localhost:4000/api/users/getUsers");
+            const json = await response.json();
+
+            if (!response.ok) {
+                console.log(json.error)
+            }
+
+            if (response.ok) {
+                setUsers(json);
+            }
+        }
+
+        fetchUsers();
+    }, []);
+
     return (
         <div>
-            
+            <Nav />
+
+            <h1 className='text-center text-2xl font-bold mt-10'>All Users</h1>
+
+            <div className='mt-10 w-96 mx-auto'>
+                <div className='flex justify-between'>
+                    <p className='font-bold text-2xl'>Email</p>
+                    <p className='font-bold text-2xl'>Username</p>
+                </div>
+                {
+                    users && users.map(user => (
+                        <Div key={user._id} user={user} />
+                    ))
+                }
+            </div>
         </div>
     );
 }
