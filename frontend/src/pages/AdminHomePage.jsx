8942bd353b0ced5af41c5addd922420ed04f1215
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-07 15:37:59 -0400

    fetch books from db

--- frontend/src/pages/AdminHomePage.jsx
+++ frontend/src/pages/AdminHomePage.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useContext } from 'react';
+import BookCard from '../components/BookCard';
 import Nav from '../components/Nav';
+import BookContext from '../context/BookContext';
 
 function AdminHomePage() {
+
+    const { dispatch, books} = useContext(BookContext);
+
+
+    useEffect(() => {
+        const fetchBooks = async () => {
+            const response = await fetch("http://localhost:4000/api/books");
+            const json = await response.json();
+
+            if (!response.ok) {
+                console.log(json.error)
+            }
+
+            if (response.ok) {
+                dispatch({type: "FETCH BOOKS", payload: json})
+            }
+        }
+
+        fetchBooks();
+    }, [dispatch])
+
     return (
         <div>
            <Nav />
+
+           {
+            books && books.map(item => (
+                <BookCard key={item._id} item={item} />
+            ))
+           }
         </div>
     );
 }
