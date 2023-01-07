b648007331ae51a914935d2c511120ae26f28d22
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-06 23:12:37 -0400

    added login route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import AdminHomePage from './pages/AdminHomePage'
+import Login from './pages/Login'
 import Register from './pages/Register'
 import UserHomePage from './pages/UserHomePage'
 
@@ -13,6 +14,7 @@ function App() {
         <Route index element={<UserHomePage />} />
         <Route path='/admin' element={<AdminHomePage />} />
         <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
       </Route>
      </Routes>
     </div>
