489f7226e35f39a7a659c8524655cb522e24f367
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-06 22:23:12 -0400

    updated signup body

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -6,10 +6,10 @@ const loginUser = async (req, res) => {
 };
 
 const signupUser = async (req, res) => {
-  const body = req.body;
+  const { email, password, userName } = req.body;
 
   try {
-    const user = await User.signup(body);
+    const user = await User.signup(email, password, userName);
     res.status(200).json(user);
   } catch (error) {
     res.status(400).json({ error: error.message });
--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -17,12 +17,12 @@ const userSchema = new Schema({
   },
 });
 
-userSchema.statics.signup = async function (email, password, username) {
-  if (!email || !password || !username) {
+userSchema.statics.signup = async function (email, password, userName) {
+  if (!email || !password || !userName) {
     throw new Error("All fields must be filled");
   }
 
-  const exists = this.findOne({ email });
+  const exists = await this.findOne({ email });
 
   if (exists) {
     throw new Error("User already exists");
@@ -31,7 +31,7 @@ userSchema.statics.signup = async function (email, password, username) {
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(password, salt);
 
-  const user = await this.create({ email, password: hash });
+  const user = await this.create({ email, password: hash, userName });
 
   return user;
 };
