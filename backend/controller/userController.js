302151a758f9f2aa9ca29bcc584c4b28b3f38499
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-06 22:05:06 -0400

    Initialized signup

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -4,3 +4,18 @@ const loginUser = async (req, res) => {
   try {
   } catch (error) {}
 };
+
+const signupUser = async (req, res) => {
+  const body = req.body;
+
+  try {
+    const user = await User.signup(body);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  signupUser,
+};
