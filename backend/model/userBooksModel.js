0e7c5986f7d45a34e6031d62a93542849cdbe032
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-08 13:57:41 -0400

    completed user books model

---
+++ backend/model/userBooksModel.js
@@ -0,0 +1,18 @@
+const { Schema, default: mongoose } = require("mongoose");
+
+const userBooksSchema = new Schema({
+  authorName: {
+    type: String,
+    required: true,
+  },
+  bookName: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("userBooks", userBooksSchema);
