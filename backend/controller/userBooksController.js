56aa7794227b1f53e18b5c0e1469db605ca35dd1
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-08 14:36:02 -0400

    updated user books controller

---
+++ backend/controller/userBooksController.js
@@ -0,0 +1,35 @@
+const UserBooks = require("../model/userBooksModel");
+
+const getUserBooks = async (req, res) => {
+  try {
+    const books = await UserBooks.find({});
+    res.status(200).json(books);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+const addToCollection = async (req, res) => {
+  const { authorName, bookName, image } = req.body;
+
+  try {
+    const exists = await UserBooks.findOne({ authorName, bookName, image });
+
+    if (exists) {
+      return res
+        .status(400)
+        .json({ error: "Book already exists in your collection" });
+    }
+
+    const book = await UserBooks.create({ authorName, bookName, image });
+
+    res.status(200).json(book);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  getUserBooks,
+  addToCollection,
+};
