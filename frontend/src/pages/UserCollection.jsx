b82bbf9df0291402896cf2f3189081773732edce
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-08 15:02:30 -0400

    added topnav to  user collection page

---
+++ frontend/src/pages/UserCollection.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function UserCollection() {
+    return (
+        <div>
+            <TopNav />
+            
+        </div>
+    );
+}
+
+export default UserCollection;
