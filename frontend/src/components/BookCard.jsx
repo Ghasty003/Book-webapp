f736395214769c1be678b872da27d43d20f5c496
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-08 14:19:17 -0400

    added border radius to book image

--- frontend/src/components/BookCard.jsx
+++ frontend/src/components/BookCard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function BookCard({ book }) {
     return (
         <div className='flex w-56 items-center my-2 py-4 bg-primary rounded-lg drop-shadow-2xl text-white'>
-            <img className='w-20 mx-3 h-20 object-contain drop-shadow-2xl' src={ book.image } alt="book" />
+            <img className='w-20 mx-3 h-20 object-contain drop-shadow-2xl rounded-2xl' src={ book.image } alt="book" />
             <div>
                 <p className='font-bold text-lg'>{ book.authorName }</p>
                 <p className='text-sm whitespace-nowrap'>{ book.bookName }</p>
