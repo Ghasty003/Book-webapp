f59ad1c2e95976ad0ee47afa05d9373fae154871
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-08 14:56:36 -0400

    added user collection context to main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,16 +4,19 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { AuthContextProvider } from './context/AuthContext'
 import { BookContextProvider } from './context/BookContext'
+import { UserCollectionContextProvider } from './context/UserCollectionContext'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <AuthContextProvider>
     <BookContextProvider>
-      <BrowserRouter>
-        <React.StrictMode>
-          <App />
-        </React.StrictMode>
-      </BrowserRouter>
+      <UserCollectionContextProvider>
+        <BrowserRouter>
+          <React.StrictMode>
+            <App />
+          </React.StrictMode>
+        </BrowserRouter>
+      </UserCollectionContextProvider>
     </BookContextProvider>
   </AuthContextProvider>,
 )
