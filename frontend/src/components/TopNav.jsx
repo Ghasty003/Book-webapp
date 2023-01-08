19fc8647343d4f342b173774158c16a2630282ce
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-07 22:18:10 -0400

    changed nav file postion

---
+++ frontend/src/components/TopNav.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function TopNav() {
+    return (
+        <nav>
+            <div>
+                <h1>Book Web</h1>
+                <button onClick={handleLogout} className='bg-red-300 px-5 py-2 rounded-lg shadow-2xl drop-shadow-2xl duration-200 active:scale-90'>Logout</button>
+            </div>
+            <ul>
+
+            </ul>
+        </nav>
+    );
+}
+
+export default TopNav;
