c10f5cba98e64926c65f3e1dc9365c2f1fb53d97
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-07 21:25:57 -0400

    added input search box

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import { useState } from 'react';
+import { useContext } from 'react';
+import Nav from '../components/Nav';
+import BookContext from '../context/BookContext';
 
 function DeleteBook() {
+
+    const [query, setQuery] = useState("");
+
+    const { books } = useContext(BookContext);
+
+    const deletebook = books.filter(book => {
+        return book.bookName === query
+    });
+
     return (
         <div>
-            
+            <Nav />
+
+            <h1 className='text-center text-2xl font-bold mt-10'>Search for books to delete.</h1>
+
+            <form className='mt-10 flex items-center justify-center bg-primary w-80 p-4 rounded-lg mx-auto'>
+                <input className='outline-none border-none rounded px-2 py-1' type="text" value={query} onChange={e => setQuery(e.target.value)} />
+                <button>search</button>
+            </form>
         </div>
     );
 }
