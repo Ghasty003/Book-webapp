9828f755cf34de9d98aecbe3d6051df59675abb9
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-09 12:40:02 -0400

    added delete account logic to frontend

--- frontend/src/pages/UserSettings.jsx
+++ frontend/src/pages/UserSettings.jsx
@@ -1,14 +1,42 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import TopNav from '../components/TopNav';
 import AuthContext from '../context/AuthContext';
 import { FaUser } from "react-icons/fa";
 import { BsKeyFill } from "react-icons/bs";
 import { MdEmail, MdDelete } from "react-icons/md";
 
-const Lists = ({ text, index }) => {
+const Lists =  ({ text, index, setResponse }) => {
 
-    const handleClick = () => {
-        
+    const { user, dispatch } = useContext(AuthContext);
+
+    const handleClick = async () => {
+        if (index == 3) {
+           const response = await deleteUser();
+           console.log(response);
+           setResponse(response);
+           setTimeout(() => {
+            localStorage.removeItem("user");
+            dispatch({type: "LOGOUT"});
+           }, 2000);
+        }
+    }
+
+    function deleteUser() {
+       return new Promise( async (resolve, reject) => {
+        const response = await fetch("http://localhost:4000/api/users/"+ user.userId, {
+            method: "DELETE"
+        });
+
+        const json = await response.json();
+
+        if (!response.ok) {
+            reject(json.error);
+        }
+
+        if (response.ok) {
+            resolve("Account deleted successfully.");
+        }
+       })
     }
 
     return (
@@ -20,6 +48,8 @@ const Lists = ({ text, index }) => {
 }
 
 function UserSettings() {
+
+    const [response, setResponse] = useState("");
 
     const { user } = useContext(AuthContext);
 
@@ -33,8 +63,17 @@ function UserSettings() {
                 <Lists text="Change Username" index="0" />
                 <Lists text="Change Password" index="1" />
                 <Lists text="Change Email address" index="2" />
-                <Lists text="Delete account" index="3" />
+                <Lists setResponse={setResponse} text="Delete account" index="3" />
             </ul>
+
+            {
+            response && (
+                <div className='fixed bottom-8 border border-l-red-500 border-l-2 animate-bounce timing rounded-md py-3 px-6 left-[50%] -translate-x-[50%] flex items-center gap-2 bg-white shadow-2xl'>
+                    <div className='bg-red-500 rounded-full p-1 text-white'><MdDelete /></div>
+                    <div>{ response }</div>
+                </div>
+            )
+          }
         </div>
     );
 }
