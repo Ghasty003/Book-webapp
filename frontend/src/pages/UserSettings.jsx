7b48212e7713167d1a41a04903263812ea940ca6
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-09 13:45:19 -0400

    added delete account logic to frontend

--- frontend/src/pages/UserSettings.jsx
+++ frontend/src/pages/UserSettings.jsx
@@ -5,52 +5,34 @@ import { FaUser } from "react-icons/fa";
 import { BsKeyFill } from "react-icons/bs";
 import { MdEmail, MdDelete } from "react-icons/md";
 
-const Lists =  ({ text, index, setResponse }) => {
-
-    const { user, dispatch } = useContext(AuthContext);
-
-    const handleClick = async () => {
-        if (index == 3) {
-           const response = await deleteUser();
-           setResponse(response);
-           setTimeout(() => {
-            localStorage.removeItem("user");
-            dispatch({type: "LOGOUT"});
-           }, 2000);
-        }
-    }
-
-    function deleteUser() {
-       return new Promise( async (resolve, reject) => {
-        const response = await fetch("http://localhost:4000/api/users/"+ user.userId, {
-            method: "DELETE"
-        });
-
-        const json = await response.json();
-
-        if (!response.ok) {
-            reject(json.error);
-        }
-
-        if (response.ok) {
-            resolve("Account deleted successfully.");
-        }
-       })
-    }
-
-    return (
-        <div className='flex items-center gap-2'>
-            {index == 0 ? <FaUser size={20} /> : index == 1 ? <BsKeyFill size={20} /> : index == 2 ? <MdEmail size={20} /> : <MdDelete color='red' size={20} />}
-            <li onClick={handleClick} index={index} className={`${index == 3 ? "text-red-600" : ""} cursor-pointer`}>{ text }</li>
-        </div>
-    )
-}
-
 function UserSettings() {
 
     const [response, setResponse] = useState("");
+    const [showMessage, setShowMessage] = useState(false);
 
-    const { user } = useContext(AuthContext);
+    const { user, dispatch } = useContext(AuthContext);
+
+    const handleDelete = () => {
+        setShowMessage(true);
+    }
+
+    function deleteUser() {
+        return new Promise( async (resolve, reject) => {
+         const response = await fetch("http://localhost:4000/api/users/"+ user.userId, {
+             method: "DELETE"
+         });
+ 
+         const json = await response.json();
+ 
+         if (!response.ok) {
+             reject(json.error);
+         }
+ 
+         if (response.ok) {
+             resolve("Account deleted successfully.");
+         }
+        })
+     }
 
     return (
         <div>
@@ -59,20 +41,48 @@ function UserSettings() {
             <h1 className='text-center text-2xl font-bold mt-10'>Hi, {user.userName}</h1>
 
             <ul className='flex flex-col gap-6 ml-44 mt-14'>
-                <Lists text="Change Username" index="0" />
-                <Lists text="Change Password" index="1" />
-                <Lists text="Change Email address" index="2" />
-                <Lists setResponse={setResponse} text="Delete account" index="3" />
+                <div className='flex items-center gap-2'>
+                    <FaUser size={20} />
+                    <li className={`cursor-pointer`}>Change Username</li>
+                </div>
+
+                <div className='flex items-center gap-2'>
+                    <BsKeyFill size={20} />
+                    <li className={`cursor-pointer`}>Change Password</li>
+                </div>
+
+                <div className='flex items-center gap-2'>
+                    <MdEmail size={20} />
+                    <li onClick={handleDelete} className={`cursor-pointer`}>Change Email Address</li>
+                </div>
+
+                <div className='flex items-center gap-2'>
+                    <MdDelete color='red' size={20} />
+                    <li onClick={handleDelete} className={`cursor-pointer text-red-600`}>Delete account</li>
+                </div>
             </ul>
 
-            <div className='fixed top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 text-center bg-yellow-100 py-6 px-10 shadow-xl rounded-lg'>
-                <h2 className='font-bold text-xl my-4'>Are you sure you want to delete account?</h2>
-                <p className='my-2 font-bold'>This action is irreversible</p>
-                <div className='flex gap-4 justify-end mt-4'>
-                    <button className='bg-red-300 text-white py-1 px-6 rounded-lg'>Yes</button>
-                    <button>Cancel</button>
+           {
+            showMessage && (
+                <div className='fixed top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 text-center bg-yellow-100 py-6 px-10 shadow-xl rounded-lg'>
+                    <h2 className='font-bold text-xl my-4'>Are you sure you want to delete account?</h2>
+                    <p className='my-2 font-bold'>This action is irreversible</p>
+                    <div className='flex gap-4 justify-end mt-4'>
+                        <button onClick={async () => {
+                            const response = await deleteUser();
+                            setResponse(response);
+                            setShowMessage(false);
+                            setTimeout(() => {
+                            localStorage.removeItem("user");
+                            dispatch({type: "LOGOUT"});
+                            }, 2000);
+                        }} className='bg-red-300 text-white py-1 px-6 rounded-lg'>Yes</button>
+                        
+                        <button onClick={() => setShowMessage(false)}>Cancel</button>
+                    </div>
                 </div>
-            </div>
+            )
+           }
 
             {
             response && (
