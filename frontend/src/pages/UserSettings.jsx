e3111c27dfbb5ba8b957ce5894dd5eb66483fe28
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-09 11:13:05 -0400

    added top nav to user settings page

---
+++ frontend/src/pages/UserSettings.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TopNav from '../components/TopNav';
+
+function UserSettings() {
+    return (
+        <div>
+            <TopNav />
+            
+        </div>
+    );
+}
+
+export default UserSettings;
