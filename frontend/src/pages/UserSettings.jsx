7b51a6308ff317b2f4674e23a31f1b0edfeae239
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-09 11:18:38 -0400

    added username to top of settings page

--- frontend/src/pages/UserSettings.jsx
+++ frontend/src/pages/UserSettings.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import TopNav from '../components/TopNav';
+import AuthContext from '../context/AuthContext';
 
 function UserSettings() {
+
+    const { user } = useContext(AuthContext);
+
     return (
         <div>
             <TopNav />
             
+            <h1 className='text-center text-2xl font-bold mt-10'>Hi, {user.userName}</h1>
         </div>
     );
 }
